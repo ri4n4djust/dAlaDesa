76ac78849b7c772e1cc0e6a0bec96e6c139a9cf9
ri4n4djust <rian.adjust@gmail.com>
2026-02-06 17:25:18 +0800

    update

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -5,13 +5,13 @@
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
-  <link rel="stylesheet" href="{{asset('assets/bower_components/bootstrap/dist/css/bootstrap.min.css')}}">
+  <link rel="stylesheet" href="{{asset('assets/bower_components/bootstrap/dist/css/bootstrap.css')}}">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{asset('assets/bower_components/font-awesome/css/font-awesome.min.css')}}">
+  <link rel="stylesheet" href="{{asset('assets/bower_components/font-awesome/css/font-awesome.css')}}">
   <!-- Ionicons -->
-  <link rel="stylesheet" href="{{asset('assets/bower_components/Ionicons/css/ionicons.min.css')}}">
+  <link rel="stylesheet" href="{{asset('assets/bower_components/Ionicons/css/ionicons.css')}}">
   <!-- Theme style -->
-  <link rel="stylesheet" href="{{asset('assets/dist/css/AdminLTE.min.css')}}">
+  <link rel="stylesheet" href="{{asset('assets/dist/css/AdminLTE.css')}}">
 
   <!-- bootstrap datepicker -->
   <!-- AdminLTE Skins. Choose a skin from the css/skins
@@ -31,9 +31,13 @@
   
 
   <!-- Google Font -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700,300italic,400italic,600italic">
 
   <style>
+    .form-control {
+      border-radius: 0px;
+      font-size: 2em;
+    }
     .modal-mask {
      position: fixed;
      z-index: 9998;
